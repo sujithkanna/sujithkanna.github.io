325da469dcc9be6d167dbb39a4ac7226017c96d9
Sujith <sujith.niraikulathan@gmail.com>
2020-06-24 23:08:51 +0530

    Align top bar to grid

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "4124c774de936fc1368847de4d3988dc",
 "/": "4124c774de936fc1368847de4d3988dc",
-"main.dart.js": "e908a0ac72129a7bbc2ba59c8e54c72b",
+"main.dart.js": "0ff8e6480fb0538b8cd2a259510922c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
